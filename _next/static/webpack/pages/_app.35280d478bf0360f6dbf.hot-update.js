57718d7e6007f0d87c883704f4aac92de13ebf73
landpy <landpy@outlook.com>
2021-06-05 01:38:46 +0800

    Refine code.

---
+++ _next/static/webpack/pages/_app.35280d478bf0360f6dbf.hot-update.js
@@ -0,0 +1,17 @@
+webpackHotUpdate_N_E("pages/_app",{
+
+/***/ "./svgs/iphone-mockup.svg":
+/*!********************************!*\
+  !*** ./svgs/iphone-mockup.svg ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "static/media/iphone-mockup.3276179d992f7ab3baae0e2d7c7e3d5f.svg");
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vc3Zncy9pcGhvbmUtbW9ja3VwLnN2ZyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O0FBQUE7QUFBZSxvRkFBdUIsb0VBQW9FLEUiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svcGFnZXMvX2FwcC4zNTI4MGQ0NzhiZjAzNjBmNmRiZi5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgX193ZWJwYWNrX3B1YmxpY19wYXRoX18gKyBcInN0YXRpYy9tZWRpYS9pcGhvbmUtbW9ja3VwLjMyNzYxNzlkOTkyZjdhYjNiYWFlMGUyZDdjN2UzZDVmLnN2Z1wiOyJdLCJzb3VyY2VSb290IjoiIn0=
---
+++ _next/static/webpack/pages/_app.997d3df942e5ee27b980.hot-update.js
@@ -0,0 +1,17 @@
+webpackHotUpdate_N_E("pages/_app",{
+
+/***/ "./svgs/iphone-mockup.svg":
+/*!********************************!*\
+  !*** ./svgs/iphone-mockup.svg ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "static/media/iphone-mockup.5681fd20e4d206aa340b9c4c6d3df357.svg");
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vc3Zncy9pcGhvbmUtbW9ja3VwLnN2ZyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O0FBQUE7QUFBZSxvRkFBdUIsb0VBQW9FLEUiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svcGFnZXMvX2FwcC45OTdkM2RmOTQyZTVlZTI3Yjk4MC5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgX193ZWJwYWNrX3B1YmxpY19wYXRoX18gKyBcInN0YXRpYy9tZWRpYS9pcGhvbmUtbW9ja3VwLjU2ODFmZDIwZTRkMjA2YWEzNDBiOWM0YzZkM2RmMzU3LnN2Z1wiOyJdLCJzb3VyY2VSb290IjoiIn0=
