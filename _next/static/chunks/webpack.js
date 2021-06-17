8579420c300cf5bc1024ddc1cd0a2de65149ab36
XL Pang <mv@live.cn>
2021-06-17 12:57:42 +0800

    Add banner.

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c14a9834d4d634fa5a44";
+/******/ 	var hotCurrentHash = "008c59a1a52084b43783";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
