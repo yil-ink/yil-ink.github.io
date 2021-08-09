141b8f1ce5fd9e1bc235a8ca900bbe9d167a899a
XL Pang <mv@live.cn>
2021-08-09 11:04:58 +0800

    Refine UI.

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "13cf22c3aa5d58eaa293";
+/******/ 	var hotCurrentHash = "abd45979c2bd7188a74f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
